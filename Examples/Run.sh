344f88a98af534b20e1c24ea685654e31a130f1d
Shule Yu <ysl6.626@gmail.com>
2018-01-17 17:24:26 -0700

    sc

--- Examples/Run.sh
+++ Examples/Run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ $# -ne 1 ] || ! [ -e $1 ]
 then
     echo "Usage: input c code as \$1."
@@ -7,7 +9,7 @@ then
 fi
 
 SRCDIR=${PWD}
-CCODEDIR=${HOME}/Research/Fun.C.c002
+CCODEDIR=${PWD}/..
 INCLUDE="-I${CCODEDIR} -I${SACHOME}/include -I/opt/local/include -I/usr/include"
 LIBDIR="-L${CCODEDIR} -L${SACHOME}/lib -L/opt/local/lib"
 LIBs="-lASU_tools -lsac -lsacio -lgsl -lgslcblas -lfftw3 -lm "
@@ -20,14 +22,9 @@ cd ${SRCDIR}
 
 gcc -Wall -Wimplicit $1 ${INCLUDE} ${LIBDIR} ${LIBs}
  
-
 # ======== Run c code ========
-if [ $? -ne 0 ]
-then
-    echo "$1 compile failed ..."
-    exit 1
-fi
 
 time ./a.out
+rm -f a.out
 
 exit 0
