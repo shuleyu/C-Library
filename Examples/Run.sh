3c666daf1cf44f7e2694ccd82ec8202aee02d6b8
Shule Yu <ysl6.626@gmail.com>
2018-03-31 10:28:05 -0700

    BugFix - Makefile

--- Examples/Run.sh
+++ Examples/Run.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 
-set -e
-
-if [ $# -ne 1 ] || ! [ -e $1 ]
-then
-    echo "Usage: input c code as \$1."
-    exit 1;
-fi
+[ $# -ne 1 ] || ! [ -e $1 ] && echo "Usage: input c code as \$1." && exit 1;
+CodeName=$1
 
 SRCDIR=${PWD}
 CCODEDIR=${PWD}/..
-INCLUDE="-I${CCODEDIR} -I${SACHOME}/include -I/opt/local/include -I/usr/include"
-LIBDIR="-L${CCODEDIR} -L${SACHOME}/lib -L/opt/local/lib"
-LIBs="-lASU_tools -lsac -lsacio -lgsl -lgslcblas -lfftw3 -lm "
-
 cd ${CCODEDIR}
-make
+make examples
+[ $? -ne 0 ] && exit 1
+
 cd ${SRCDIR}
+time ./${CodeName%c}out
 
-# ======== Compile c code ========
-
-gcc -fPIC -Wall -Wimplicit $1 ${INCLUDE} ${LIBDIR} ${LIBs}
- 
-# ======== Run c code ========
-
-time ./a.out
 echo ""
-
-rm -f a.out
 
 exit 0
